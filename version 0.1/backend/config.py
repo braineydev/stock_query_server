77acfb3515702313002e964240d7839ef3061070
abrahams-code <64852135+abrahams-code@users.noreply.github.com>
2026-03-23 12:09:23 +0300

    Clear email credentials in config.py
    Removed sensitive email credentials from config.

--- version 0.1/backend/config.py
+++ version 0.1/backend/config.py
@@ -7,5 +7,5 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'clozeabu@gmail.com'
-    MAIL_PASSWORD = 'Mrmoney@1'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
